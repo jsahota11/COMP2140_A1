4b6a7e5d3494cd2c7ed617bfb9dbc38d6c80eb7e
Jatinder Sahota <jsahota751@gmail.com>
2024-09-20 08:30:54 -0500

    Add hasTargetSum() and hasThreeSum.

--- SahotaJatinderA1Q4/src/Main.java
+++ SahotaJatinderA1Q4/src/Main.java
@@ -2,14 +2,37 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+
+    public int[] readFile(){
+        return null;
+    }
+
+    public boolean hasTargetSum(int[] A, int j, int k, int t){
+        boolean answer = false;
+        while ((j<k)&&(!answer)){
+            if(A[j] + A[k] < 1){
+                j = j+1;
+            } else if (A[j] + A[k] > 1){
+                k = k-1;
+            } else {
+                answer = true;
+            }
+        }
+        return answer;
+    }
+
+    public boolean hasThreeSum(int[] A){
+        int n = A.length;
+        int i = 0;
+        boolean answer = false;
+
+        while ((i < n-2)^(!answer)){
+            answer = hasTargetSum(A,i+1,n-1,-A[i]);
+            i = i+1;
+        }
+
+        return answer;
     }
 }
